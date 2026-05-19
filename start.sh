aa8e7955938f452877c6b80b22357fe17fe884ab
Will-Dance-Prosser <William.dance-prosser@theaa.com>
2026-05-19 09:57:20 +0100

    Fix start.sh line endings (LF) and add .gitattributes

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Running database migrations..."
+echo 'Running database migrations...'
 flask db upgrade
 
-echo "Seeding demo users..."
+echo 'Seeding demo users...'
 flask seed-demo-users
 
-echo "Seeding demo MDM data..."
+echo 'Seeding demo MDM data...'
 flask seed-demo-mdm
 
-echo "Starting gunicorn..."
+echo 'Starting gunicorn...'
 exec gunicorn wsgi:app
