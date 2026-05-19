70bbd9bb94cddb84714139f58d95a4b3552866ca
Will-Dance-Prosser <William.dance-prosser@theaa.com>
2026-05-19 10:03:50 +0100

    Run migrations and seed from wsgi.py on startup

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
+from flask_migrate import upgrade
 from app import create_app
+from app.extensions import db
 
 app = create_app()
 
-#hosting
+with app.app_context():
+    upgrade()
+    from app.commands import seed_demo_users, seed_demo_mdm
+    from click.testing import CliRunner
+    runner = CliRunner()
+    runner.invoke(seed_demo_users)
+    runner.invoke(seed_demo_mdm)
